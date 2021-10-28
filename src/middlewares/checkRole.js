86f6ae11c5f9a9856532ab3376aa18a020a05311
Ying Yin Chang <yingyinchang.com@gmail.com>
2021-10-28 12:27:30 -0700

    finished the checkRole middleware

--- src/middlewares/checkRole.js
+++ src/middlewares/checkRole.js
@@ -1,10 +1,17 @@
-const checkRole = async (req, res, next, roleName) => {
-  try {
-    console.log(roleName);
-    console.log(req.user)
-  } catch (e) {
-    res.status(401).json({ error: 'Please authenticate.' });
-  }
+const jwt = require('jsonwebtoken');
+const checkRole = (roleNames) => {
+	return async function (req, res, next) {
+		try {
+			const token = req.header('Authorization').replace('Bearer ', '');
+			const decoded = jwt.verify(token, process.env.SECRET);
+			if (!roleNames.includes(decoded.role.name)) {
+				throw new Error('You are not authorized to perform this action');
+			}
+			next();
+		} catch (e) {
+			res.status(403).json({ error: e.message });
+		}
+	};
 };
 
 module.exports = checkRole;
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const checkRole = require('../middlewares/checkRole');
 
 const userController = require('../controllers/users');
 const { validate, getUsers, createNewUser, deleteUser } = userController;
 const validateRules = require('../middlewares/validators/validateRules');
 
 router.get('/', getUsers);
-router.post('/', validate('createNewUser'), validateRules, createNewUser);
-router.delete('/:userId', validate('deleteUser'), validateRules, deleteUser)
+router.post(
+	'/',
+	checkRole(['super admin']),
+	validate('createNewUser'),
+	validateRules,
+	createNewUser,
+);
+router.delete(
+	'/:userId',
+	checkRole.bind(null, ['super admin']),
+	validate('deleteUser'),
+	validateRules,
+	deleteUser,
+);
 
 module.exports = router;
